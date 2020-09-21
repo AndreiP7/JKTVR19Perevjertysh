7b04b75cde89d1c5510bd00b724358e762207bfe
AndreiKlimin1 <61226190+AndreiKlimin1@users.noreply.github.com>
2020-09-21 15:14:31 +0300

    перевертыш

--- src/jktvr19perevjertysh/App.java
+++ src/jktvr19perevjertysh/App.java
@@ -5,6 +5,9 @@
  */
 package jktvr19perevjertysh;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Administrator
@@ -12,6 +15,29 @@ package jktvr19perevjertysh;
 public class App {
     public void run() {
         System.out.println("---Перевертыш---");
-        
+        System.out.println("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("В строке символов: " + str.length());
+        System.out.print("Перевертыш: ");
+        char[] chStr = str.toCharArray();
+        char[] pChStr = new char[str.length()];
+        for (int i = 0; i < pChStr.length; i++) {
+            pChStr[i] = chStr[(pChStr.length-1) - i];
+            
+        }
+        System.out.println(Arrays.toString(pChStr));
+        String pStr = new String(pChStr);
+        System.out.println(pStr);
+        System.out.print("Второе слово этой строки: ");
+        int firstSpace = str.indexOf(" ");
+        int secondSpace = str.indexOf(" ",firstSpace+1);
+        if (firstSpace < 0) System.out.println("Нет второго слова.");
+        else if (secondSpace < 0){
+            System.out.println(str.substring(firstSpace+1));
+        }else {
+            System.out.println(str.substring(firstSpace+1, secondSpace));
+        }
+        /*System.out.println("НО-НО!");*/
     }
 }
